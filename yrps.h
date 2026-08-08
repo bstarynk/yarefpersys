e4730bf8106ed2b5f1627a1cd744653d408640f4
Basile STARYNKEVITCH <basile@starynkevitch.net>
2026-08-08 22:15:25 +0200

    added comments, even in French, in yrps.h
    Signed-off-by: Basile STARYNKEVITCH <basile@starynkevitch.net>

--- yrps.h
+++ yrps.h
@@ -28,7 +28,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <unistr.h> 
+#include <unistr.h>
 #include <readline/readline.h>
 #include <curl/curl.h>
 #include <dlfcn.h>
@@ -36,28 +36,44 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+/// maximal path of files 
 #ifndef YRPS_PATHMAX
+/*€ longueur maximale des noms de fichiers */
 #define YRPS_PATHMAX 384
 #endif /*YRPS_PATHMAX */
 
+// maximal width in bytes 
 #ifndef YRPS_LINEWIDTHMAX
+/*€ longueur max (octets) d'une ligne de fichier */
 #define YRPS_LINEWITHMAX 256
 #endif
 
+// maximal number of lines in any textual files 
 #ifndef YRPS_LINECOUNTMAX
+/*€ nombre maximum de lignes dans nos fichiers textuels, y compris en C */
 #define YRPS_LINECOUNTMAX 4096
 #endif
 
+
+/// main arguments
+/*€ argument du main et du programme */
 extern int yrps_argc;
 extern char *const *yrps_argv;
+
+// self program handle (dlopen)
+/*€ poignée vers le programme tout entier (dlopen) */
 extern void *yrps_proghdl;
+
+// self code and/or data directory path
+/*€ le repertoire contenant le source et/ou les données textuelles */
 extern char yrps_dirpath[YRPS_PATHMAX];
 
-// check that a directory is readable
+// check that a directory is readable and convenient
+/*€ vérifie qu'un repertoire est lisible et convenable */
 extern bool yrps_readable_directory (const char *dirpath);
 
 // check that a file is textual, no more than YRPS_LINECOUNTMAX lines &
 // YRPS_LINEWIDTHMAX bytes per line, each line being UTF8
 // on error stderr gets a message
-extern bool yrps_check_valid_textual_file(const char*path);
+extern bool yrps_check_valid_textual_file (const char *path);
 #endif //YRPS_INCLUDED
