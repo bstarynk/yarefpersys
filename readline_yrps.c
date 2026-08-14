6c66bb2ebec1c9934bb63a9a80f89cafe43694c2
Basile STARYNKEVITCH <basile@starynkevitch.net>
2026-08-14 08:56:20 +0200

    stubbing readline_yrps.c
    Signed-off-by: Basile STARYNKEVITCH <basile@starynkevitch.net>

---
+++ readline_yrps.c
@@ -0,0 +1,32 @@
+// file yarefpersys/readline_yrps.c
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+/***
+ * License:
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****/
+#include "yrps.h"
+
+const char readline_yrps_id[] = YRPS_ID;
+
+
+void
+yrps_init_readline (void)
+{
+#warning unimplemented yrps_init_readline
+  YRPS_UNIQUE_BREAKPOINT ();
+}				/* end yrps_init_readline */
+
+///// eof readline_yrps.c [€fin du fichier]
