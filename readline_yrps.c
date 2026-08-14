3aec6f5e1060579a19fac28e9af7beb4ad2d70cb
Basile STARYNKEVITCH <basile@starynkevitch.net>
2026-08-14 15:07:27 +0200

    stubbed readline_completion_yrps
    Signed-off-by: Basile STARYNKEVITCH <basile@starynkevitch.net>

--- readline_yrps.c
+++ readline_yrps.c
@@ -21,12 +21,25 @@
 
 const char readline_yrps_id[] = YRPS_ID;
 
+static char *readline_completion_yrps (const char *, int);
 
 void
 yrps_init_readline (void)
 {
 #warning unimplemented yrps_init_readline
   YRPS_UNIQUE_BREAKPOINT ();
+  fprintf (stderr, "yrps_init_readline [%s:%d] unimplemented\n",
+	   __FILE__, __LINE__ - 1);
+  rl_completion_entry_function = readline_completion_yrps;
 }				/* end yrps_init_readline */
+
+
+char *
+readline_completion_yrps (const char *str, int key)
+{
+  YRPS_UNIQUE_BREAKPOINT ();
+  YRPS_PRINTFAIL ("readline_completion_yrps str=%s key=%d unimplemented\n",
+		  str, key);
+}				/* end readline_completion_yrps */
 
 ///// eof readline_yrps.c [€fin du fichier]
