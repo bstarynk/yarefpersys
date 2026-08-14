6ae0dec1e05908b5629aff59f6b4b86a18506943
Basile STARYNKEVITCH <basile@starynkevitch.net>
2026-08-14 16:39:05 +0200

    shorter generated gitid
    Signed-off-by: Basile STARYNKEVITCH <basile@starynkevitch.net>

--- yrps-shortgit.sh
+++ yrps-shortgit.sh
@@ -4,7 +4,7 @@
 #
 # © Copyright (C) 2026, Basile Starynkevitch
 yrps_getid="$(git log --format=oneline -q -1 \
-		| cut '-d ' -f 1 | head -16c \
+		| cut '-d ' -f 1 | head -10c \
 		| tr -d '\n')"
 
 printf "%s\n" $yrps_getid
